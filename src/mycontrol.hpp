d655a792bb43028e73c0d0f1a1799b2d1998a465
Ben Morse <bjmorse@gmail.com>
2022-09-11 18:01:13 -0700

    some fsm basics

--- src/mycontrol.hpp
+++ src/mycontrol.hpp
@@ -7,11 +7,15 @@ enum control_state_t {
     STATE_READ,
 };
 
+enum control_ev_t {
+    EV_RESET,
+};
+
 struct control_t {
     Servo myservo;
     long int last_char_time = 0;
 
-    myfsm_t<control_state_t> fsm;
+    myfsm_t<control_state_t, control_ev_t> fsm;
 
     void setup(void);
     void handleChar2(void);
--- src/myfsm.hpp
+++ src/myfsm.hpp
@@ -4,25 +4,36 @@
 
 typedef void (*cb_t)(int prev);
 
-template <typename T> struct myfsm_t {
+template <typename ST_T, typename EV_T>
+struct myfsm_t {
 
-    T* ptr;
+    // T* ptr;
     int size;
 
     std::vector<int> list;
+    std::vector<EV_T> ev_q;
 
     // Array(T arr[], int s);
     // void print();
     void init(int f);
+    void post(const EV_T ev);
+    void tick(void);
 };
 
 
 
-template <typename T> void myfsm_t<T>::init(int f) {
+template <typename ST_T, typename EV_T>
+void myfsm_t<ST_T,EV_T>::init(int f) {
     printf("in init\r\n");
     list.push_back(f);
 
     for( const auto w : list) {
         printf("%d\n", w);
     }
+}
+
+
+template <typename ST_T, typename EV_T>
+void myfsm_t<ST_T,EV_T>::post(const EV_T ev) {
+    ev_q.push_back(ev);
 }
